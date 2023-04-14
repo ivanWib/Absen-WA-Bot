9bfd5858da038e0d5abfe5072aed893beeecc84b
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-04-14 13:49:29 +0700

    membuat folder untuk gambar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const qrcode = require("qrcode-terminal");
-const fs = require("fs");
-const { Client } = require("whatsapp-web.js");
-const { Prisma } = require("@prisma/client");
+import qrcode from "qrcode-terminal";
+import { writeFileSync } from "fs";
+import { Client } from "whatsapp-web.js";
+import { Prisma } from "@prisma/client";
 const client = new Client();
 
 client.on("qr", (qr) => {
@@ -25,16 +25,21 @@ client.on("message", async (message) => {
   if (message.hasMedia) {
     const media = await message.downloadMedia();
     const mimeType = media.mimetype.split("/")[1];
-    const filename = `${Date.now()}.${mimeType}`;
+    // const filename = `${Date.now()}.${mimeType}`;
+
+    const filename = `${message.body}.${mimeType}`;
 
     // Menyimpan data gambar dalam format Base64 ke file
-    fs.writeFileSync(filename, media.data, { encoding: "base64" });
+    // writeFileSync(filename, media.data, { encoding: "base64" });
 
     // Membaca data gambar dari file dan mendecode dari format Base64 ke gambar
     const decodedImage = Buffer.from(media.data, "base64");
 
     // Simpan gambar ke file
-    fs.writeFileSync(`decoded_${filename}`, decodedImage);
+    // fs.writeFileSync(`decoded_${filename}`, decodedImage);
+
+    // simpan gambar ke folder uploads
+    writeFileSync(`uploads/${filename}`, decodedImage);
 
     console.log(`Gambar ${filename} berhasil didownload dan dideskripsi!`);
   }
